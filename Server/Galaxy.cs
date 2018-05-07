515c07829bd9844d72222039db1cb366cc8548d9
kistnj <kistnj@miamioh.edu>
2018-05-06 20:30:20 -0400

    fixed bugg in universe class

---
+++ Server/Galaxy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPDServer {
+    class Galaxy {
+
+        private char star;
+        private char blackhole;
+        private char treasure;
+        private char planet;
+        private char[] cells = new char[100];
+
+        public Galaxy() {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 99; i++) {
+                cells[i] = (char) rnd.Next(30, 127);
+            }
+
+        }
+
+        public char GetCell(int location) {
+            return cells[location];
+        }  
+
+        public char Star { get => star; set => star = value; }
+        public char Blackhole { get => blackhole; set => blackhole = value; }
+        public char Treasure { get => treasure; set => treasure = value; }
+        public char Planet { get => planet; set => planet = value; }
+    }
+}
---
+++ Server/Universe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPDServer {
+    class Universe {
+
+        Universe() {
+            Dictionary<String, Galaxy> galaxies = new Dictionary<String, Galaxy>();
+
+            char sec = 'A';
+            while (sec != 'Q') {
+                for (int i = 0; i < 16; i++) {
+                    galaxies.Add(sec + "" + i, new Galaxy());
+
+                }
+                sec++;
+            }
+        }
+
+        
+
+
+    }
+}
