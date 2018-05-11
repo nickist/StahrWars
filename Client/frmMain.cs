598da08d2701877e6f230f0a93bef14b384db493
kistnj <kistnj@miamioh.edu>
2018-05-10 20:53:14 -0400

    stuff

--- Client/frmMain.cs
+++ Client/frmMain.cs
@@ -21,7 +21,7 @@ namespace Client
         Image planet = Image.FromFile("jupiter.png");
         Image star = Image.FromFile("star.png");
         Image background = Image.FromFile("background.jpg");
-
+        Image blackhole = Image.FromFile("blackhole.jpg");
         Image shipNorth = Image.FromFile("ShipNorth.png");
         Image shipSouth = Image.FromFile("ShipSouth.png");
         Image shipEast = Image.FromFile("ShipEast.png");
